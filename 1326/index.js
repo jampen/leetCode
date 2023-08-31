f4392e8131082c52a521ce167b2c352e40e22f95
jampen <pendergastj@proton.me>
2023-08-31 12:28:37 +0100

    added comments and refactored the code to improve functionality

--- 1326/index.js
+++ 1326/index.js
@@ -1,3 +1,10 @@
+/**
+ * For clamping  a number between min and max
+ * @param {number} min
+ * @param {number} max
+ * @param {number} value
+ * @return {number}
+ **/
 const clamper = function (min, max) {
   return (value) => {
     if (value < min) return min;
@@ -20,22 +27,30 @@ const minTaps = (n, ranges) => {
     the count of uniquely watering taps.
   */
 
-  const clamp = clamper(0, n);
+  const taps = (() => {
+    const clamp = clamper(0, n);
 
-  const taps = ranges
-    .map((range, point) => {
-      return { range, point };
-    })
-    .filter(({ range }) => range !== 0)
-    // Sort the taps by range in descending order, this is key to the solution.
-    .sort((left, right) => right.range - left.range)
-    .map(({ range, point }) => {
-      return { start: clamp(point - range), end: clamp(point + range) };
-    });
+    return (
+      ranges
+        .map((range, point) => {
+          return { range, point };
+        })
+        .filter(({ range }) => range !== 0)
+        // Sort the taps by range in descending order, this is key to the solution.
+        .sort((left, right) => right.range - left.range)
+        .map(({ range, point }) => {
+          return { start: clamp(point - range), end: clamp(point + range) };
+        })
+    );
+  })();
 
+  // The number of taps which have watered at least 1 space
   let numEffectiveTaps = 0;
+
+  // For keeping track of where we have watered
   let visited = new Array(n).fill(false);
 
+  // Water the garden
   taps.forEach(({ start, end }) => {
     let wateredAtLeastOneSpace = false;
 
@@ -51,7 +66,7 @@ const minTaps = (n, ranges) => {
     }
   });
 
-  //  one space is not watered
+  //  At least one space is not watered
   if (visited.includes(false)) return -1;
   else return numEffectiveTaps;
 };
